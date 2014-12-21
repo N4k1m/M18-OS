f950814fefdb42777b9119f19b175e7ffd797878
Xavier Mawet <mawet.xavier@gmail.com>
2014-12-21 15:22:16 +0100

    Java : Ajout de la classe ProCrypto

---
+++ Java/MyLittleCheapLibrary/src/SPF/Crypto/DES/ProCrypto.java
@@ -0,0 +1,25 @@
+package SPF.Crypto.DES;
+
+import SPF.Crypto.Chiffrement;
+import SPF.Crypto.CryptoProvider;
+
+/**
+ *
+ * @author nakim
+ */
+public class ProCrypto implements CryptoProvider
+{
+    // <editor-fold defaultstate="collapsed" desc=" Constructeur ">
+    public ProCrypto()
+    {
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Overrided methods">
+    @Override
+    public Chiffrement newChiffrement()
+    {
+        return new CryptoDES();
+    }
+    // </editor-fold>
+}
