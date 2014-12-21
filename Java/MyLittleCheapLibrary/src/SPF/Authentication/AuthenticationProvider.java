703dd0fa6c5a4f858345106926f234852cd352c5
Xavier Mawet <mawet.xavier@gmail.com>
2014-12-21 12:31:44 +0100

    Java : Ajout de l'interface AuthenticationProvider

---
+++ Java/MyLittleCheapLibrary/src/SPF/Authentication/AuthenticationProvider.java
@@ -0,0 +1,12 @@
+package SPF.Authentication;
+
+/**
+ *
+ * @author nakim
+ */
+
+// Interface provider de service (Authentication)
+public interface AuthenticationProvider
+{
+    Authentication newAuthentication();
+}
