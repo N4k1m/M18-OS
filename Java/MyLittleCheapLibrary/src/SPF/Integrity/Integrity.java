660801afd5e452056c279f7c6ed6cc7722a46e40
Xavier Mawet <mawet.xavier@gmail.com>
2014-12-21 15:24:05 +0100

    Java : Ajout de l'interface Integrity

---
+++ Java/MyLittleCheapLibrary/src/SPF/Integrity/Integrity.java
@@ -0,0 +1,18 @@
+package SPF.Integrity;
+
+import SPF.ByteArrayList;
+
+/**
+ *
+ * @author nakim
+ */
+
+// Interface de service
+public interface Integrity
+{
+    byte[] makeCheck(String plainText);
+    byte[] makeCheck(String plainText, ByteArrayList salt);
+    boolean verifyCheck(String plainText, byte[] digest);
+    boolean verifyCheck(String plainText, ByteArrayList salt, byte[] digest);
+    String getProvider();
+}
