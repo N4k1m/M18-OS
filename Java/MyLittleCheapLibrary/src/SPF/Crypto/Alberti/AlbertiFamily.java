57c906a1cb608a00bb32520b846b36396ba81e2a
Xavier Mawet <mawet.xavier@gmail.com>
2014-12-21 15:09:00 +0100

    Java : Ajout de la classe AlbertiFamily

---
+++ Java/MyLittleCheapLibrary/src/SPF/Crypto/Alberti/AlbertiFamily.java
@@ -0,0 +1,25 @@
+package SPF.Crypto.Alberti;
+
+import SPF.Crypto.Chiffrement;
+import SPF.Crypto.CryptoProvider;
+
+/**
+ *
+ * @author nakim
+ */
+public class AlbertiFamily implements CryptoProvider
+{
+    // <editor-fold defaultstate="collapsed" desc=" Constructeur ">
+    public AlbertiFamily()    
+    {
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc=" Overrided methods ">
+    @Override
+    public Chiffrement newChiffrement() 
+    {
+        return new ChiffrePoly();
+    }
+    // </editor-fold>
+}
