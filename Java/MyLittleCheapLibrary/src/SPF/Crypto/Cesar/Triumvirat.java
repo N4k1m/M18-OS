84c4be65b4f682bebbf15ddac51bc638af7202e4
Xavier Mawet <mawet.xavier@gmail.com>
2014-12-21 15:13:39 +0100

    Java : Ajout de la classe Triumvirat

---
+++ Java/MyLittleCheapLibrary/src/SPF/Crypto/Cesar/Triumvirat.java
@@ -0,0 +1,25 @@
+package SPF.Crypto.Cesar;
+
+import SPF.Crypto.Chiffrement;
+import SPF.Crypto.CryptoProvider;
+
+/**
+ *
+ * @author nakim
+ */
+public class Triumvirat implements CryptoProvider
+{
+    // <editor-fold defaultstate="collapsed" desc="Constructeur">
+    public Triumvirat() 
+    {
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Overrided methods">
+    @Override
+    public Chiffrement newChiffrement() 
+    {
+        return new CryptoCaesar();
+    }
+    // </editor-fold>
+}
