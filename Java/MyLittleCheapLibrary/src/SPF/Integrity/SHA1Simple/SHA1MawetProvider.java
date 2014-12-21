7ea9983891604787f51a45e4030030ddf7008226
Xavier Mawet <mawet.xavier@gmail.com>
2014-12-21 15:30:38 +0100

    Java : Ajout de la classe SHA1MawetProvider

---
+++ Java/MyLittleCheapLibrary/src/SPF/Integrity/SHA1Simple/SHA1MawetProvider.java
@@ -0,0 +1,27 @@
+package SPF.Integrity.SHA1Simple;
+
+import SPF.Integrity.Integrity;
+import SPF.Integrity.IntegrityProvider;
+
+/**
+ *
+ * @author nakim
+ */
+public class SHA1MawetProvider implements IntegrityProvider
+{
+    // <editor-fold defaultstate="collapsed" desc=" Constructeur ">
+
+    public SHA1MawetProvider()
+    {
+    }
+
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Overrided methods">
+    @Override
+    public Integrity newIntegrity()
+    {
+        return new SHA1Mawet();
+    }
+    // </editor-fold>
+}
