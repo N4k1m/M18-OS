821ffb1ae74e3051699a4305de8633e66a80ab01
Xavier <mawet.xavier@gmail.com>
2014-12-22 11:30:44 +0100

    Ajout de DS_Store au gitignore

--- Java/MyLittleCheapLibrary/src/Main/Main.java
+++ Java/MyLittleCheapLibrary/src/Main/Main.java
@@ -16,9 +16,6 @@ import java.util.Date;
  */
 public class Main
 {
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws Exception
     {
         System.out.println("-------------------------------------------------");
