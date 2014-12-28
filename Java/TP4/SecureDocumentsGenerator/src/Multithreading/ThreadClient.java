bb62da91f98d4db90e5caf5cf16db50d42bada01
Xavier <mawet.xavier@gmail.com>
2014-12-28 12:54:34 +0100

    Java TP4.2 : Ajout de la classe ThreadClient

---
+++ Java/TP4/SecureDocumentsGenerator/src/Multithreading/ThreadClient.java
@@ -0,0 +1,63 @@
+package Multithreading;
+
+import GMC.TaskQueue;
+
+/**
+ *
+ * @author Nakim
+ */
+public class ThreadClient extends Thread
+{
+    //<editor-fold defaultstate="collapsed" desc="Constructor">
+    public ThreadClient(String name, TaskQueue tasks)
+    {
+        super(name);
+
+        this.tasks = tasks;
+        this.currentTask = null;
+        this.isStopped = false;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Overrided methods">
+    @Override
+    public void run()
+    {
+        while(!this.isStopped())
+        {
+            try
+            {
+                this.currentTask = tasks.dequeue();
+                this.currentTask.run();
+            }
+            catch (InterruptedException ex)
+            {
+                //log or otherwise report exception,
+                System.err.println(ex);
+
+                //but keep pool thread alive.
+            }
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Private methods">
+    public synchronized void requestStop()
+    {
+        this.isStopped = true;
+        this.interrupt(); // Break client thread out of dequeue() call
+    }
+
+    public synchronized boolean isStopped()
+    {
+        return this.isStopped;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Private variables">
+    private TaskQueue tasks;
+    private Runnable currentTask;
+
+    private boolean isStopped;
+    //</editor-fold>
+}
